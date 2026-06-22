d5e0b35661bb6ea0777ce6cf5f3814547ff480ba
IsraelCastroDev <israelcastro.dev@gmail.com>
2026-06-22 14:58:39 -0500

    fix: agregar validación de plan

--- src/Model/Categoria.php
+++ src/Model/Categoria.php
@@ -380,6 +380,11 @@ class Categoria extends Mysql
             $proyectoGeneral = new Proyectogeneral($args);
             $result = $proyectoGeneral->save();
             error_log("Proyecto general guardado: " . json_encode($result));
+
+            if (!$result['success']) {
+                throw new Exception($result['message']);
+            }
+
             $proyectoId = $result["data"];
             error_log("id proyecto general guardado: " . $proyectoId);
 
