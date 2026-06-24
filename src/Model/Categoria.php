d8b73d1f6fb58f62b0816fa449b2ba2cb9d7d71c
IsraelCastroDev <israelcastro.dev@gmail.com>
2026-06-24 15:58:07 -0500

    refactor: limpiando mas celdas

--- src/Model/Categoria.php
+++ src/Model/Categoria.php
@@ -1203,12 +1203,14 @@ class Categoria extends Mysql
         for ($i = 5; $i <= 8; $i++) {
             $sheet->setCellValue("B{$i}", null);
             $sheet->setCellValue("D{$i}", null);
+            $sheet->setCellValue("E{$i}", null);
         }
 
         // Limpiar ambientes (filas 16-68, columnas B y C)
         for ($i = 16; $i <= 68; $i++) {
             $sheet->setCellValue("B{$i}", null);
             $sheet->setCellValue("C{$i}", null);
+            $sheet->setCellValue("D{$i}", null);
         }
 
         // Limpiar exteriores (filas 72-83, columnas B y C)
@@ -1216,6 +1218,7 @@ class Categoria extends Mysql
             $sheet->setCellValue("B{$i}", null);
             $sheet->setCellValue("C{$i}", null);
             $sheet->setCellValue("D{$i}", null);
+            $sheet->setCellValue("E{$i}", null);
         }
     }
 }
