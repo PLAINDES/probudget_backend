780a7485f24aa8411f37fcc6b25e997ddcfa4bad
IsraelCastroDev <israelcastro.dev@gmail.com>
2026-07-13 12:06:30 -0500

    refactor: quitar log

--- src/Controllers/CategoriaController.php
+++ src/Controllers/CategoriaController.php
@@ -40,7 +40,6 @@ class CategoriaController
 
     public function crearPresupuestoColegio($request)
     {
-        error_log("REQUEST: " . json_encode($request));
         $categoria   = new Categoria();
         return $categoria->crearPresupuestoColegio($request);
     }
